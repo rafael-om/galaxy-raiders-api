57a5f90b166f37910ca4b4ad907c6544920a5204
rafael-om <rafaelom04@gmail.com>
2023-05-17 18:03:04 -0300

    Solução EP1 - MAC0218

--- app/src/main/kotlin/galaxyraiders/core/physics/Point2D.kt
+++ app/src/main/kotlin/galaxyraiders/core/physics/Point2D.kt
@@ -1,13 +1,20 @@
 @file:Suppress("UNUSED_PARAMETER") // <- REMOVE
 package galaxyraiders.core.physics
+import kotlin.math.abs
+import kotlin.math.pow
+import kotlin.math.sqrt
 
 data class Point2D(val x: Double, val y: Double) {
   operator fun plus(p: Point2D): Point2D {
-    return INVALID_POINT
+    var new_x: Double = this.x + p.x
+    var new_y: Double = this.y + p.y
+    return Point2D(new_x, new_y)
   }
 
   operator fun plus(v: Vector2D): Point2D {
-    return INVALID_POINT
+    var new_x: Double = this.x + v.dx
+    var new_y: Double = this.y + v.dy
+    return Point2D(new_x, new_y)
   }
 
   override fun toString(): String {
@@ -15,26 +22,28 @@ data class Point2D(val x: Double, val y: Double) {
   }
 
   fun toVector(): Vector2D {
-    return INVALID_VECTOR
+    return Vector2D(this.x, this.y)
   }
 
   fun impactVector(p: Point2D): Vector2D {
-    return INVALID_VECTOR
+    var dx: Double = abs(this.x - p.x)
+    var dy: Double = abs(this.y - p.y)
+    return Vector2D(dx, dy)
   }
 
   fun impactDirection(p: Point2D): Vector2D {
-    return INVALID_VECTOR
+    return this.impactVector(p).unit
   }
 
   fun contactVector(p: Point2D): Vector2D {
-    return INVALID_VECTOR
+    return this.impactVector(p).normal
   }
 
   fun contactDirection(p: Point2D): Vector2D {
-    return INVALID_VECTOR
+    return this.contactVector(p).unit
   }
 
   fun distance(p: Point2D): Double {
-    return INVALID_DOUBLE
+    return sqrt((this.x - p.x).pow(2) + (this.y - p.y).pow(2))
   }
 }
--- app/src/main/kotlin/galaxyraiders/core/physics/Vector2D.kt
+++ app/src/main/kotlin/galaxyraiders/core/physics/Vector2D.kt
@@ -2,6 +2,12 @@
 package galaxyraiders.core.physics
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import kotlin.math.PI
+import kotlin.math.atan
+import kotlin.math.sqrt
+import java.util.Locale
+
+const val ANGLE = 180.0
 
 @JsonIgnoreProperties("unit", "normal", "degree", "magnitude")
 data class Vector2D(val dx: Double, val dy: Double) {
@@ -10,57 +16,88 @@ data class Vector2D(val dx: Double, val dy: Double) {
   }
 
   val magnitude: Double
-    get() = INVALID_DOUBLE
+    get() = sqrt(this.dx * this.dx + this.dy * this.dy)
 
   val radiant: Double
-    get() = INVALID_DOUBLE
+    get() {
+      val tan = this.dy / this.dx
+      var angle = atan(tan)
+      // Arco tangente está definido entre -PI/2 e +PI/2
+      // Se ângulo está no 3° quadrante é mandando para o 1°
+      if (tan > 0 && this.dx < 0) return (angle - PI)
+      // Se ângulo está no 2° quadrante é mandando para o 4°
+      else if (tan < 0 && this.dx < 0) return (angle + PI)
+      return angle
+    }
 
   val degree: Double
-    get() = INVALID_DOUBLE
+    get() = this.radiant * ANGLE / PI
 
   val unit: Vector2D
-    get() = INVALID_VECTOR
+    get() = this.div(this.magnitude)
 
   val normal: Vector2D
-    get() = INVALID_VECTOR
+    get() = Vector2D(this.unit.dy, -this.unit.dx)
 
   operator fun times(scalar: Double): Vector2D {
-    return INVALID_VECTOR
+    var new_dx: Double = this.dx * scalar
+    var new_dy: Double = this.dy * scalar
+    return Vector2D(new_dx, new_dy)
   }
 
   operator fun div(scalar: Double): Vector2D {
-    return INVALID_VECTOR
+    var new_dx: Double = this.dx / scalar
+    var new_dy: Double = this.dy / scalar
+    return Vector2D(new_dx, new_dy)
   }
 
   operator fun times(v: Vector2D): Double {
-    return INVALID_DOUBLE
+    return v.dx * this.dx + v.dy * this.dy
   }
 
   operator fun plus(v: Vector2D): Vector2D {
-    return INVALID_VECTOR
+    var new_dx: Double = this.dx + v.dx
+    var new_dy: Double = this.dy + v.dy
+    return Vector2D(new_dx, new_dy)
   }
 
   operator fun plus(p: Point2D): Point2D {
-    return INVALID_POINT
+    var new_x: Double = p.x + this.dx
+    var new_y: Double = p.y + this.dy
+    return Point2D(new_x, new_y)
   }
 
   operator fun unaryMinus(): Vector2D {
-    return INVALID_VECTOR
+    var new_dx: Double = -this.dx
+    var new_dy: Double = -this.dy
+    return Vector2D(new_dx, new_dy)
   }
 
   operator fun minus(v: Vector2D): Vector2D {
-    return INVALID_VECTOR
+    var new_dx: Double = this.dx - v.dx
+    var new_dy: Double = this.dy - v.dy
+    return Vector2D(new_dx, new_dy)
   }
 
   fun scalarProject(target: Vector2D): Double {
-    return INVALID_DOUBLE
+    return this.times(target) / target.magnitude
   }
 
   fun vectorProject(target: Vector2D): Vector2D {
-    return INVALID_VECTOR
+    var coef: Double = this.scalarProject(target)/target.magnitude
+    val vector: Vector2D = target.times(coef)
+    // Está ocorrendo um erro em um dos testes por causa de arredondamento e 
+    //um colega (Vitor de Melo) conseguiu resolver com o código abaixo:
+    val result = Vector2D(
+      String.format(Locale.US, "%.2f",vector.dx).toDouble(),
+      String.format(Locale.US, "%.2f",vector.dy).toDouble()
+    )
+    return result
   }
 }
 
 operator fun Double.times(v: Vector2D): Vector2D {
-  return INVALID_VECTOR
+  var new_dx: Double = this * v.dx
+  var new_dy: Double = this * v.dy
+  return Vector2D(new_dx, new_dy)
 }
